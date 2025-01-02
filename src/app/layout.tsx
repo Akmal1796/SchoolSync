45f6858daf7046dca2d5be23eaec1c6c4e0c0765
Akmal1796 <mhdakmal1796@gmail.com>
2025-01-02 21:07:26 +0530

    Completed the dashboard top cards

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
+  title: "SchooDB",
   description: "Next.js School Management System",
 };
 
